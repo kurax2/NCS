33832b0d92d98d141fe7dd6c361c74691a0ad7aa
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-06-09 12:15:11 +0530

    Add files via upload

---
+++ p6/AirportDB.java
@@ -0,0 +1,16 @@
+package p6;
+
+public class AirportDB {
+	
+	public static String allAirports[] = new  String[5];
+	
+	static {
+		allAirports[0] = "Singapore";
+		allAirports[1] = "New Delhi";
+		allAirports[2] = "Hong Kong";
+		allAirports[3] = "Shingai";
+		allAirports[4] = "KL-Airport";
+		
+	}
+
+}
--- p6/Flight.java
+++ p6/Flight.java
@@ -2,6 +2,7 @@ package p6;
 
 public class Flight {
 	
+	private String flightCode; // always starts from 3 alphabatic characters ABC123 valid , 123 invalid , ab12 invalid
 	private String flightName;
 	private Passenger passengers[];
 	private String sourceAirport;
