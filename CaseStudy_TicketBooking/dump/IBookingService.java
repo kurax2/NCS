7db7fc5ce44dec94817a15bdc4a26b41ff408e29
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-06-16 13:48:58 +0530

    Update IBookingService.java

--- CaseStudy_TicketBooking/dump/IBookingService.java
+++ CaseStudy_TicketBooking/dump/IBookingService.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public interface IBookingService {
 	
+	
+	
 	public Ticket doBookTickect(int count)throws TicketCountException;
 	public List<Ticket> showAllTickets();
 	public Ticket getDetailsById(int bookingId)throws InvalidBookingId;
 }
+
+// in implementation class 
+// List<Booking> allBookings;
