625594334fb73b66bdce19b6b244d4f07e704cd4
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-07-04 07:49:57 +0530

    Add files via upload

---
+++ SpringCore-Demo1/src/main/java/p1/Employee.java
@@ -0,0 +1,104 @@
+package p1;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+public class Employee implements Comparable<Employee>,Serializable{
+	
+
+	private int empId;
+	private String name;
+	private String email;
+	private int bankAccount;
+	private String designation;
+	private int salary;
+	
+
+	//-----------------------------------------------------------------------
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return this.empId - o.empId;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(int bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", email=" + email + ", bankAccount=" + bankAccount
+				+ ", designation=" + designation + ", salary=" + salary + "]";
+	}
+	
+	
+	
+	
+	
+
+}
