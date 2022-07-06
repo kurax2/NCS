2cd8466d6be28b4bc1856f30e177f67a2d03f2b2
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-07-06 14:43:59 +0530

    Add files via upload

--- EmployeeConsolePro/src/main/java/com/ncs/empconsole/dao/EmployeeDB.java
+++ EmployeeConsolePro/src/main/java/com/ncs/empconsole/dao/EmployeeDB.java
@@ -14,25 +14,34 @@ import com.ncs.empconsole.model.Employee;
 @Component
 public class EmployeeDB {
 	
-	public static List<Employee> employeeDB = new ArrayList<>();
+	public List<Employee> employeeDB = new ArrayList<>();
 
-	static
+	EmployeeDB()
 	{
 		Employee e1 = new Employee(101, "Ramesh", 2000);
 		Employee e2 = new Employee(102, "Suresh", 3000);
 		Employee e3 = new Employee(103, "Mahesh", 1500);
 		Employee e4 = new Employee(104, "Rakesh", 7000);
 	
-		employeeDB = Arrays.asList(e1,e2,e3,e4);
+		employeeDB.add(e1);
+		employeeDB.add(e2);
+		employeeDB.add(e3);
+		employeeDB.add(e4);
+		
 	}
 
-	public static boolean addEmployee(Employee e)
+	public boolean addEmployee(Employee e)
 	{
-		return employeeDB.add(e);
+		System.out.println("--->> 2) list.size() :- "+employeeDB.size() + " - "+e);
+		boolean status =  employeeDB.add(e);
+		
+		System.err.println("--->> 3) inside Employee DB "+status);
+		
+		return status;
 		
 	}
 	
-	public static Employee getEmployee(int id)
+	public Employee getEmployee(int id)
 	{
 		List<Employee> list =   employeeDB.stream().filter((e)->{
 			if(e.getEmpId() == id)
@@ -47,7 +56,7 @@ public class EmployeeDB {
 		
 	}
 	
-	public static Employee getEmployee(String name)
+	public Employee getEmployee(String name)
 	{
 		List<Employee> list =   employeeDB.stream().filter((e)->{
 			if(e.getName().equalsIgnoreCase(name))
@@ -63,7 +72,7 @@ public class EmployeeDB {
 	}
 	
 	
-	public static Employee updateProjectInfo(int searchId,int projectId)
+	public Employee updateProjectInfo(int searchId,int projectId)
 	{
 		
 		
@@ -80,13 +89,11 @@ public class EmployeeDB {
 		return null;
 	}
 	
-	public static List<Employee> getEmployeeDB() {
+	public List<Employee> getEmployeeDB() {
 		return employeeDB;
 	}
 
-	public static void setEmployeeDB(List<Employee> employeeDB) {
-		EmployeeDB.employeeDB = employeeDB;
-	}
+	
 	
 	
 	
