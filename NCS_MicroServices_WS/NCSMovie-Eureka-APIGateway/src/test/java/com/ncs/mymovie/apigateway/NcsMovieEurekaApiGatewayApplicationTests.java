70bba814b4ea27306b98ce6562d1c362443afc82
fsdtrinings <87074240+fsdtrinings@users.noreply.github.com>
2022-07-13 13:29:05 +0530

    Add files via upload

---
+++ NCS_MicroServices_WS/NCSMovie-Eureka-APIGateway/src/test/java/com/ncs/mymovie/apigateway/NcsMovieEurekaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncs.mymovie.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcsMovieEurekaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
